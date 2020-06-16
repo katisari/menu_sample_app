7521c431df3a914cfcd4a15def3f9ffa23aa5fe1
Katie Lee <ktjlee@umich.edu>
2020-06-16 15:17:19 -0400

    few fixes

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class MenuItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Text(icon,
-          style: TextStyle(
-            fontSize: 30.0,
-          )),
+      leading: Text(
+        icon,
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
       title: Text(itemText),
     );
   }
@@ -27,55 +29,50 @@ class Menu extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Menu Demo'),
-          backgroundColor: Colors.cyan,
+          title: Text('Menu Demo: Solution'),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Row(children: [
-                // Error 1: Overflow, surround Text with expanded to fix error
-                Expanded(
-                  child: Text(
-                      'Welcome to Dasheats, a restaurant providing fast, '
-                      'affordable, and quality experience. '
-                      'Explore the delicious menu items below.',
-                      style: TextStyle(fontSize: 15.0,)),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            Row(
+              children: [
+                // Issue 1: Overflow error fix by wrapping Text with Expanded
+                // Issue caused because Text is unconstrained, so it goes off the screen
+                Text(
+                  'Explore our restaurant\'s delicious menu items!',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
                 ),
-              ]),
+              ],
             ),
-            // Error 2: RenderBox not laid out error, surround ListView with Expanded to fix error
             Expanded(
               child: ListView(
                 children: [
                   MenuItem('🥤', 'Soda'),
-                  MenuItem('🍔', 'Burgers'),
-                  MenuItem('🍟', 'Fries'),
+                  MenuItem('🍔', 'Burger'),
                 ],
               ),
             ),
             Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FlatButton(
-                    onPressed: null,
-                    child: Text(
-                      'Pickup',
-                    ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Pickup',
                   ),
-                  // Error 3: Unable to see the VerticalDivider despite being in the code
-                  // The divider does not cause any error messages
-                  Container(child: VerticalDivider(thickness: 2.0,), height: 15.0,),
-                  FlatButton(
-                    onPressed: null,
-                    child: Text(
-                      'Delivery',
-                    ),
-                  )
-                ],
-              )
-          ],
+                ),
+                VerticalDivider(),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Delivery',
+                  ),
+                )
+              ],
+            )
+          ]),
         ),
       ),
     );
