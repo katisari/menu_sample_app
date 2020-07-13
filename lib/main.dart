fd953000fb5a15f889376beab643362dacdbfdfb
Katie Lee <ktjlee@umich.edu>
2020-07-12 23:54:54 -0400

    added padding around example1

--- lib/main.dart
+++ lib/main.dart
@@ -33,89 +33,80 @@ class Menu extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.all(20),
-          child: Column(children: [
-            // Examples are put here!
-            Example1(),
-            Example2(),
-            Example3(),
-          ]),
+          child: Column(
+            children: [
+              // Modify and Insert code here
+              Example1(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// Fixed Issue 1: Overflow Error, Wrap Text in Expanded
-// Cause: Text was unconsrained, leading it to go off screen
-// Solution: Wrap Text in Exapnded
+// Issue 1: Overflow Error
 class Example1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Text(
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30.0),
+      child: Row(
+        children: [
+          Text(
             'Explore the restaurant\'s delicious menu items below!',
             style: TextStyle(
               fontSize: 18.0,
             ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+}
+
+// Issue 2: Viewport was given unbounded height Error
+class Example2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        MenuItem('🍔', 'Burger'),
+        MenuItem('🍟', 'Fries'),
+        MenuItem('🥤', 'Soda'),
       ],
     );
   }
 }
 
-// Fixed Issue 2: RenderBox not laid out error
-// Cause : ListView was taking up infinity height so program couldn't determine its size
-// Solution: Wrap ListView in Expanded
-class Example2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        children: [
-          MenuItem('🍔', 'Burger'),
-          MenuItem('🍟', 'Fries'),
-          MenuItem('🥤', 'Soda'),
-        ],
-      ),
-    );
-  }
-}
-
-// Fixed Issue 3: VerticalDivider not shown
-// Cause: Vertical Divider height was 0 because Row's height was unconstrained
-// Solution: Wrap Row in SizedBox and give it a height
+// Issue 3: VerticalDivider not shown
 class Example3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 48.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          RaisedButton(
-            onPressed: () {
-              print('Pickup button pressed.');
-            },
-            child: Text(
-              'Pickup',
-            ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        RaisedButton(
+          onPressed: () {
+            print('Pickup button pressed.');
+          },
+          child: Text(
+            'Pickup',
           ),
-          VerticalDivider(
-            thickness: 5.0,
+        ),
+        // This widget is not shown on screen initially.
+        VerticalDivider(
+          thickness: 5.0,
+        ),
+        RaisedButton(
+          onPressed: () {
+            print('Delivery button pressed.');
+          },
+          child: Text(
+            'Delivery',
           ),
-          RaisedButton(
-            onPressed: () {
-              print('Delivery button pressed.');
-            },
-            child: Text(
-              'Delivery',
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
